248994e1c869e3b454c36aeb7ab33ed964a61e4b
Trần Quang Bảo Quý <quyeinsteinit@gmail.com>
2026-02-07 22:00:14 +0700

    Quy v0.0.1

--- Assets/Scripts/Movement/InputManager.cs
+++ Assets/Scripts/Movement/InputManager.cs
@@ -1,18 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static InputManager Instance;
+
+    // Movement
+    public Vector2 MoveInput { get; private set; }
+    public bool JumpPressed { get; private set; }
+
+    // Mouse
+    public Vector2 MouseDelta { get; private set; }
+    public bool MouseLeft { get; private set; }
+    public bool MouseRight { get; private set; }
+
+    void Awake()
     {
-        
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        ReadKeyboard();
+        ReadMouse();
+    }
+
+    void ReadKeyboard()
+    {
+        float x = 0f;
+        float y = 0f;
+
+        if (Input.GetKey(KeyCode.A)) x -= 1f;
+        if (Input.GetKey(KeyCode.D)) x += 1f;
+        if (Input.GetKey(KeyCode.S)) y -= 1f;
+        if (Input.GetKey(KeyCode.W)) y += 1f;
+
+        MoveInput = new Vector2(x, y).normalized;
+
+        JumpPressed = Input.GetKeyDown(KeyCode.Space);
+    }
+
+    void ReadMouse()
+    {
+        MouseDelta = new Vector2(
+            Input.GetAxis("Mouse X"),
+            Input.GetAxis("Mouse Y")
+        );
+
+        MouseLeft = Input.GetMouseButton(0);
+        MouseRight = Input.GetMouseButton(1);
     }
 }
--- Assets/Scripts/Movement/Player3D.cs
+++ Assets/Scripts/Movement/Player3D.cs
@@ -1,18 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
+using Game.Interfaces;
 using UnityEngine;
 
-public class Player3D : MonoBehaviour
+public class Player3D : BaseEntity
 {
+    [Header("Movement Strategy")]
+    public IMovementStrategy moveStrategy;
+    public Camera mainCamera;
+
+    InputManager input;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
