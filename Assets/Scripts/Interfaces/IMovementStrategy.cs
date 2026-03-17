b2a5c6dc2d4444e29d3fdeed01626cbd40617adc
KEONGOTCUTIE <cvavietanh31@gmail.com>
2026-03-17 19:54:43 +0700

    Fixed all conflicts and updated Movement Strategy

--- Assets/Scripts/Interfaces/IMovementStrategy.cs
+++ Assets/Scripts/Interfaces/IMovementStrategy.cs
@@ -5,38 +5,24 @@ namespace Game.Interfaces
     public interface IMovementStrategy
     {
         void Move(Transform transform, Vector3 direction, float speed);
-<<<<<<< HEAD
-        void Jump(Rigidbody rb, Collider col, float jumpForce);
-    }
-=======
-<<<<<<< Updated upstream
-        void Jump(Rigidbody rb, float jumpForce);
-=======
+        // Giữ lại Collider để sau này xử lý check Grounded hoặc va chạm khi nhảy
         void Jump(Rigidbody rb, Collider col, float jumpForce);
     }
 
->>>>>>> keongot
     public class WalkingMovement : IMovementStrategy
     {
         public void Move(Transform transform, Vector3 direction, float speed)
         {
             transform.Translate(direction * speed * Time.deltaTime);
         }
-<<<<<<< HEAD
-        public void Jump(Rigidbody rb, float jumpForce)
-=======
+
         public void Jump(Rigidbody rb, Collider col, float jumpForce)
->>>>>>> keongot
         {
             if (rb != null)
             {
+                // Bạn có thể dùng 'col' ở đây để check va chạm trước khi cho phép nhảy
                 rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             }
         }
-<<<<<<< HEAD
-
-=======
->>>>>>> Stashed changes
->>>>>>> keongot
     }
 }
